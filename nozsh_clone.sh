6163254a62a6979e33083994be1044041e9596c4
nozsh <155148075+nozsh@users.noreply.github.com>
2024-07-18 16:26:32 +0300

    upd

--- nozsh_clone.sh
+++ nozsh_clone.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 git clone https://github.com/nozsh/home
+cd home && git submodule update --init --recursive && cd ..
+
 git clone https://github.com/nozsh/blog
+cd blog && git submodule update --init --recursive && cd ..
+
 git clone https://github.com/nozsh/knowledge-base kb
+cd kb && git submodule update --init --recursive && cd ..
+
 git clone https://github.com/nozsh/xcom
