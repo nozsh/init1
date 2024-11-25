533ced5ce4c2d026dc17adb5cd8a85f77f68ad13
nozsh <155148075+nozsh@users.noreply.github.com>
2024-11-25 17:12:32 +0300

    upd Clone Script

--- nozsh/nozsh_clone.sh
+++ nozsh/nozsh_clone.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-git clone https://github.com/nozsh/home
+git clone -b v2 https://github.com/nozsh/home
 cd home && git submodule update --init --recursive && cd ..
 
 git clone https://github.com/nozsh/blog
 cd blog && git submodule update --init --recursive && cd ..
 
-git clone https://github.com/nozsh/knowledge-base kb
+git clone -b v2 https://github.com/nozsh/knowledge-base kb
 cd kb && git submodule update --init --recursive && cd ..
-
-git clone https://github.com/nozsh/xcom
